6fd6e21749ee8e075d26bdb0f0b68d455521f144
Bryan Gurney <bgurney@redhat.com>
2023-03-30 19:10:58 -0400

    parser key : remove dict() calls (reset)
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/stratis_cli/_parser/_key.py
+++ src/stratis_cli/_parser/_key.py
@@ -57,40 +57,40 @@ KEY_SUBCMDS = [
     ),
     (
         "reset",
-        dict(
-            help="Reset an existing key in the kernel keyring",
-            args=[("keydesc", dict(action="store", help="key description"))],
-            mut_ex_args=[
+        {
+            "help": "Reset an existing key in the kernel keyring",
+            "args": [("keydesc", {"action": "store", "help": "key description"})],
+            "mut_ex_args": [
                 (
                     True,
                     [
                         (
                             "--keyfile-path",
-                            dict(
-                                action="store",
-                                help=(
+                            {
+                                "action": "store",
+                                "help": (
                                     "Path to the key file containing a key to reset "
                                     "in the keyring"
                                 ),
-                                dest="keyfile_path",
-                            ),
+                                "dest": "keyfile_path",
+                            },
                         ),
                         (
                             "--capture-key",
-                            dict(
-                                action="store_true",
-                                help=(
+                            {
+                                "action": "store_true",
+                                "help": (
                                     "Read key from stdin with no terminal echo or "
                                     "userspace buffer storage"
                                 ),
-                                dest="capture_key",
-                            ),
+                                "dest": "capture_key",
+                            },
                         ),
                     ],
                 )
             ],
-            func=TopActions.reset_key,
-        ),
+            "func": TopActions.reset_key,
+        },
     ),
     (
         "unset",
